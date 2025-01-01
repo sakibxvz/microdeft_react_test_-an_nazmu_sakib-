2ea917d86e4ec3618eb9a0958c826aee643e929a
sakibxvz <sakibxvz@gmail.com>
2025-01-01 13:18:27 +0600

    Fixed Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </head>
+    
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
